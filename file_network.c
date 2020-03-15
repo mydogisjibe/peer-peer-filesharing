3130a4a139684a513fcadfba63eb210a013102cb
Brent Luker <brentluker@umail.ucsb.edu>
2020-03-15 15:18:24 -0700

    Add functions for rarest-first algo
    Not yet implemeneted in actual code

--- file_network.c
+++ file_network.c
@@ -80,13 +80,17 @@ FileNetwork init_file_network(size_t file_size, char* peer_list) {
     for(int i=0;i<result.num_chunks-1;i++) {
         result.chunks[i].index = 0;
         result.chunks[i].length = CHUNK_SIZE;
+        result.chunks[i].finished_downloading = false;
+        result.chunks[i].peers_with_file = 0;
     }
     Chunk *last_chunk = result.chunks + result.num_chunks-1;
     last_chunk->index = 0;
     last_chunk->length = file_size % CHUNK_SIZE;
+    result.available_chunks_len = (result.file_size+CHUNK_SIZE*8-1)/CHUNK_SIZE/8;
 
     for(int i=0;i<MAX_DOWNLOADS;i++) {
         result.downloads[i].connection = -1;
+        result.downloads[i].available_chunks = malloc(result.available_chunks_len);
     }
 
     //Use the peer list to start establishing connections
@@ -110,6 +114,60 @@ FileNetwork init_file_network(size_t file_size, char* peer_list) {
     }
     return result;
 }
+
+void get_queue(FileNetwork *fn) {
+    for(int i=0;i<MAX_DOWNLOADS;i++) {
+        Download* download = fn->downloads + i;
+        if(download->connection == -1) continue;
+        
+        const char getList = 'L';
+        int len = write(download->connection, &getList, 1);
+        if(len < 0) {
+            perror("Error tying to request peer's chunks");
+            close(download->connection);
+            download->connection = -1;
+            continue;
+        }
+    }
+
+    for(int i=0;i<MAX_DOWNLOADS;i++) {
+        Download *download = fn->downloads + i;
+        if(download->connection == -1) continue;
+
+        int len = read(download->connection, download->available_chunks, fn->available_chunks_len);
+        if(len != fn->available_chunks_len) {
+            perror("Error trying to read peer's chunks");
+            close(download->connection);
+            download->connection = -1;
+            continue;
+        }
+
+        for(int j=0;j<fn->num_chunks;j++) {
+            if(download->available_chunks[j/8] & (1 << (j%8))) {
+                fn->chunks[j].peers_with_file++;
+            }
+        }
+    }
+
+        
+}
+
+int64_t choose_next_chunk(FileNetwork* fn, Download *download) {
+    int64_t next = -1;
+    int next_value;
+    for(int i=0;i<fn->num_chunks;i++) {
+        if(download->available_chunks[i/8] & (1 << (i%8))
+            && !(fn->chunks[i].finished_downloading)
+            && fn->chunks[i].peers_with_file < next) {
+
+            next = i;
+            next_value = fn->chunks[i].peers_with_file;
+        }
+    }
+
+   return next;
+}
+
 
 //Negative connection means error, don't use.
 Download start_download(char* ip, int port, Chunk *download_chunk, int64_t chunk_index) {
@@ -168,6 +226,82 @@ void restart_download(Download* download, Chunk* download_chunk, int64_t chunk_i
     }
 }
 
+//As an uploading peer, receive a request from a downloader and execute it if possible
+void receive_request(FileNetwork *fn, Upload *upload) {
+    char request;
+    int len = read(upload->connection, &request, 1);
+
+    if(len <= 0) {
+        if(len < 0)
+            perror("Failed to read message");
+        else
+            fprintf(stderr, "Unexpected close from downloader\n");
+        close(upload->connection);
+        upload->connection = -1;
+        return;
+    }
+
+    
+    switch(request) {
+        //Requesting a specific chunk. Semicolon to fix declaration error, please ignore
+        case 'C': ;
+            int64_t chunk_index;
+            len = read(upload->connection, &chunk_index, sizeof(int64_t));
+            if (len < sizeof(int64_t) || chunk_index >= fn->num_chunks || !fn->chunks[chunk_index].finished_downloading) {
+                if(len < 0)
+                    perror("Failed to read chunk index");
+                else
+                    fprintf(stderr, "Unexpected close from downloader, or unexpected chunk_index\n");
+                close(upload->connection);
+                upload->connection = -1;
+                return;
+            }
+            upload->current_chunk = fn->chunks + chunk_index;
+            Chunk *current_chunk = upload->current_chunk;
+
+            len = write(upload->connection, current_chunk->data, current_chunk->length);
+            if (len < 0) {
+                perror("Failed to write data to downloader");
+                close(upload->connection);
+                upload->connection = -1;
+                return;
+            }
+            upload->current_chunk->index = len;
+            break;
+
+        //Requesting a list of chunks that can be downloaded. Semicolon is just to fix error, ignore
+        case 'L': ;
+           size_t message_len = (fn->file_size+CHUNK_SIZE*8-1)/CHUNK_SIZE/8;
+           char *message = malloc(message_len);
+           for(int i=0;i<fn->num_chunks;i++) {
+               message[i/8] |= fn->chunks[i].finished_downloading << (i%8);
+           }
+           len = write(upload->connection, message, message_len);
+           if (len < 0) {
+               perror("Failed to send available chunks");
+               close(upload->connection);
+               upload->connection = -1;
+               return;
+           }
+           break;
+    }
+
+}
+
+void send_next_packet(Upload *current_upload) {
+    Chunk *current_chunk = current_upload->current_chunk;
+    char *start = current_chunk->data + current_chunk->index;
+    size_t length = current_chunk->length - current_chunk->index;
+    int len = write(current_upload->connection, start, length);
+    if(len <= 0) {
+        printf("Error: downloader lost\n");
+        close(current_upload->connection);
+        current_upload->connection = -1;
+        return;
+    }
+    current_chunk->index += len;
+}
+
 //If returns true, chunk has been downloaded and a new connection should be established.
 enum DownloadStatus read_next_packet(Download *current_download) {
     Chunk *current_chunk = current_download->current_chunk;
@@ -190,6 +324,7 @@ enum DownloadStatus read_next_packet(Download *current_download) {
     current_chunk->index += len;
     if(current_chunk->index == current_chunk->length) {
         fprintf(stderr, "Obtained a chunk successfully. Trying to get next one\n");
+        current_chunk->finished_downloading = true;
         return DONE;
     }
     printf("Packet obtained successfully. Now at %d\n", current_chunk->index);
@@ -204,6 +339,7 @@ void free_file_network(FileNetwork *to_delete) {
             write(to_delete->downloads[i].connection, &done, 1);
             close(to_delete->downloads[i].connection);
         }
+        free(to_delete->downloads[i].available_chunks);
     }
     free(to_delete->chunks);
 }
--- file_network.h
+++ file_network.h
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #define CHUNK_SIZE 1048576 //1MB
 #define MAX_DOWNLOADS 10
+#define MAX_UPLOADS 10
 
 
 
@@ -11,6 +12,8 @@ struct Chunk {
     char data[CHUNK_SIZE];
     ssize_t index;
     ssize_t length;
+    bool finished_downloading;
+    int peers_with_file;
 };
 typedef struct Chunk Chunk;
 
@@ -20,8 +23,15 @@ struct Download {
     char* ip;
     int port;
     Chunk *current_chunk;
+    char *available_chunks;
 };
 typedef struct Download Download;
+
+struct Upload {
+    int connection;
+    Chunk *current_chunk;
+};
+typedef struct Upload Upload;
 
 
 enum DownloadStatus {
@@ -36,7 +46,9 @@ struct FileNetwork {
     size_t num_chunks;
     int64_t next_unused_chunk;
     Chunk *chunks;
+    size_t available_chunks_len;
     Download downloads[MAX_DOWNLOADS];
+    Upload uploads[MAX_UPLOADS];
 };
 typedef struct FileNetwork FileNetwork;
 
@@ -47,6 +59,14 @@ FileNetwork init_file_network(size_t file_size, char *peer_list);
 
 //A negative connection number indicates connection failed, don't use
 Download start_download(char* ip, int port, Chunk *download_chunk, int64_t chunk_index);
+
+void send_next_packet(Upload *current_upload);
+
+void get_queue(FileNetwork *fn);
+
+int64_t choose_next_chunk(FileNetwork* fn, Download *download);
+
+void receive_request(FileNetwork *fn, Upload *upload);
 
 enum DownloadStatus read_next_packet(Download *current_download);
 
